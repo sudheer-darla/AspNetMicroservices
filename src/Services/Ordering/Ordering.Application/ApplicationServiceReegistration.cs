6c1a7f260c5bec88c64c9c8623fec323e6656107
Sudheer Darla <sudheerkumarddsn@gmail.com>
2022-10-16 18:48:00 +0530

    Added new class for registering services in the Ordering API

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceReegistration.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceReegistration
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+
+            return services;
+        }
+    }
+}
