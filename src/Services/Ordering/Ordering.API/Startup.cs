c98aa0269d23804a98e099ccd8d03fa342fcb3fc
Sudheer Darla <sudheerkumarddsn@gmail.com>
2022-10-22 22:18:20 +0530

    Implemented Registering Infrastructure Services on OrderingAPI startup

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Ordering.Application;
+using Ordering.Infrastructure;
 
 namespace Ordering.API
 {
@@ -21,6 +22,8 @@ namespace Ordering.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAplicationServices();
+
+            services.AddInfrastructureServices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
