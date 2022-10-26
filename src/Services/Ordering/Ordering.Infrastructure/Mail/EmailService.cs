2f02fb9c25d809e9bc65c2016c0b5963a9a5a081
Sudheer Darla <sudheerkumarddsn@gmail.com>
2022-10-26 17:13:40 +0530

    Created Add-Migrate for Orders context using EfCore

--- src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -10,15 +10,14 @@ namespace Ordering.Infrastructure.Mail
 {
     public class EmailService : IEmailService
     {
-        public EmailService(IOptions<EmailSettings> emailSettings, Logger<EmailService> logger)
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
         {
             _emailSettings = emailSettings.Value;
             _logger = logger;
         }
-
-        public EmailSettings _emailSettings { get; set; }
-        public Logger<EmailService> _logger { get; set; }
-
         public async Task<bool> SendEmail(Email email)
         {
             var client = new SendGridClient(_emailSettings.ApiKey);
